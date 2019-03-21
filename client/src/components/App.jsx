fd9c5c294c5263e16114bfed9e197a004f2e0d79
fahd-93 <fahdmajzoub13@gmail.com>
2019-03-21 12:17:34 +0100

    sign up is created

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
+import { HashRouter as Router, Route, NavLink } from 'react-router-dom';
+import SignUpForm from './SignUpForm';
+import SignInForm from './SignInForm';
+
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <h1>SAWA Project</h1>
-      </div>
+      <Router basename="/react-auth-ui/">
+        <div className="App">
+          <div className="App__Aside"></div>
+          <div className="App__Form">
+            <div className="PageSwitcher">
+              <NavLink to="/sign-in" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign In</NavLink>
+              <NavLink exact to="/" activeClassName="PageSwitcher__Item--Active" className="PageSwitcher__Item">Sign Up</NavLink>
+            </div>
+
+            <div className="FormTitle">
+              <NavLink to="/sign-in" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign In</NavLink> or <NavLink exact to="/" activeClassName="FormTitle__Link--Active" className="FormTitle__Link">Sign Up</NavLink>
+            </div>
+
+            <Route exact path="/" component={SignUpForm}>
+            </Route>
+            <Route path="/sign-in" component={SignInForm}>
+            </Route>
+          </div>
+
+        </div>
+      </Router>
     );
   }
 }
