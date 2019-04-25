8234e4e1dbadfa35130b57a6f78ee522afaf4461
fab-zio <fabrizio.musa@yahoo.com>
2019-04-25 14:10:25 +0200

    nav color changed, page 404

--- client/src/components/Notfound.jsx
+++ client/src/components/Notfound.jsx
@@ -1,16 +1,16 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { Container } from 'react-bootstrap';
+import { Jumbotron, Button, } from 'react-bootstrap';
 
 
 const Notfound = () => (
-    <div>
-        <Container>
-            <h2>
-                404 - Ops
-            </h2>
-            <Link to="/">Go back home</Link>
-        </Container>
-    </div>
+
+    <Jumbotron rounded>
+        <h1 className="text-center mt-3">Ops...</h1>
+        <h4 className="text-center mt-5">404 - PAGE NOT FOUND</h4>
+        <p className="text-center mt-5">The page you are looking for might have been removed, <br />had its name changed or is temporarily unavailable</p>
+        <div className="text-center">
+        <Button href="/" variant="outline-success" className="mx-auto mt-5">Go back home</Button>
+        </div>
+    </Jumbotron>
 )
 export default Notfound;
