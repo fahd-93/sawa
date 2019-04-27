ae4b10bb431dc8ede23e0e461e2e34beb67c834e
fab-zio <fabrizio.musa@yahoo.com>
2019-04-28 01:33:36 +0200

    searchBar and picture

--- client/src/components/ShowCampaign.jsx
+++ client/src/components/ShowCampaign.jsx
@@ -20,6 +20,12 @@ class ShowCampaign extends Component {
         this.props.getCampId(e)
     };
 
+    searchBar = (e) => {
+        e.preventDefault();
+                // let searchBar = document.getElementById("searchBar");
+
+        console.log('The link was clicked.', document.getElementById("searchBar"));
+    }
     render() {
 
 
@@ -36,6 +42,27 @@ class ShowCampaign extends Component {
         return (
 
             <div className="cam-container">
+
+
+                <div className="row justify-content-center">
+                    <div className="col-12 col-md-10 col-lg-8">
+                        <form className="card card-sm">
+                            <div className="card-body row no-gutters align-items-center">
+                                <div className="col-auto">
+                                    <i className="fas fa-search h4 text-body"></i>
+                                </div>
+                                <div className="col">
+                                    <input className="form-control form-control-lg form-control-borderless" type="search" placeholder="Search campaign" />
+                                </div>
+                                <div className="col-auto">
+                                    <button className="btn btn-lg btn-success" type="submit" onClick={this.searchBar} id="searchBar">Search</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+
+
                 <h1 className="cam-header">Existing Campaingns</h1>
                 <CardColumns >
 
